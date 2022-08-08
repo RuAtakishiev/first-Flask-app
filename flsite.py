6ffcdb21b0e2deef8cb0dc204612657a264e7b69
RuAtakishiev <atakishiev32ruslan@gmail.com>
2022-08-08 20:48:21 +0300

    Added flashed messages for contact page

--- flsite.py
+++ flsite.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask import render_template
 from flask import url_for
+from flask import flash
+from flask import request
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'qwertyuiop'
 menu = [{"name": "Установка", "url": "install-flask"},
         {"name": "Первое приложение", "url": "first-app"},
         {"name": "Обратная связь", "url": "contact"}]
@@ -16,8 +19,13 @@ def index():
 def about():
     return render_template('about.html', title='О сайте', menu=menu)
 
-@app.route("/contact")
+@app.route("/contact", methods=["POST", "GET"])
 def contact():
+    if request.method == 'POST':
+        if len(request.form['username']) > 2:
+            flash('Сообщение отправлено', category='success')
+        else:
+            flash('Ошибка отправки', category='error') 
     return render_template('contact.html', title='Обратная связь', menu=menu)
 
 if __name__ == "__main__":
